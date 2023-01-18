04091c42db6ef9cf3734ea71015e18401a80ee88
Muhammad Hafiz Azhari <80326383+mhafizazh@users.noreply.github.com>
2023-01-17 23:23:04 -0800

    Add files via upload

---
+++ FearTheTree.java
@@ -0,0 +1,14 @@
+/*Write Java statements that print the following console output:
+
+        \\\\\\\\\\\\\\\\\\\\
+        || FEAR THE TREE! ||
+        ////////////////////
+
+ */
+public class FearTheTree {
+    public static void main(String[]args){
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("|| FEAR THE TREE! ||");
+        System.out.println("////////////////////");
+    }
+}
