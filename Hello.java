7f3f5db1c566dc180e0624147718fffc1bc3e0be
Muhammad Hafiz Azhari <80326383+mhafizazh@users.noreply.github.com>
2023-01-17 23:22:36 -0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/*Write a console program in a class named Hello that prints the following console output:
+
+        Hello, world!
+        (You don't need to write any 'import' statements.)
+
+ */
+public class Hello {
+    public static void main(String[]args) {
+        System.out.println("Hello World");
+    }
+}
