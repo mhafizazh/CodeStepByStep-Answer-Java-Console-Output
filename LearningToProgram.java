718848bd19c485baff54e20eb64f1a72828b676a
Muhammad Hafiz Azhari <80326383+mhafizazh@users.noreply.github.com>
2023-01-17 23:23:47 -0800

    Add files via upload

---
+++ LearningToProgram.java
@@ -0,0 +1,25 @@
+/*Modify the following program so that it prints the following output (including the blank lines):
+
+        Hello, world!
+        I am learning to program in Java.
+        I hope it is a lot of fun!
+
+        I hope I get a good grade!
+
+        Maybe I'll change my major to computer science.
+        
+ */
+public class LearningToProgram {
+    public static void  main(String[]args){
+
+        System.out.println("Hello, world!");
+        System.out.println("I am learning to program in Java.");
+        System.out.println("I hope it is a lot of fun!");
+        System.out.println(" ");
+        System.out.println("I hope I get a good grade!");
+        System.out.println("Maybe I'll change my major to computer science.");
+
+
+
+    }
+}
